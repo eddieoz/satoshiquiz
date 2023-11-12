aa6ff5458f6db402d1368c1af06958b3ea26f231
eddieoz <osoriojr@gmail.com>
2023-11-12 17:32:51 +0200

    removed bugged qr-code

--- app/page.tsx
+++ app/page.tsx
@@ -198,7 +198,7 @@ export default function Chat() {
         </p>
       </div>
       
-      <div className="fixed bottom-0 hidden w-full flex flex-col items-right px-0 sm:flex">
+      {/* <div className="fixed bottom-0 hidden w-full flex flex-col items-right px-0 sm:flex">
         <p className="text-right text-xs text-gray-400 mr-5">
           <a
             href="lightning:LNURL1DP68GURN8GHJ7UMPW3ENGTNVD9NX2TMVDE6HYMRS9ASHX5NPFVESELDEJX"
@@ -213,7 +213,7 @@ export default function Chat() {
             />
           </a><br />⚡ Support the Quiz ⚡<br />
         </p>
-      </div>
+      </div> */}
     </main>
   );
 }
