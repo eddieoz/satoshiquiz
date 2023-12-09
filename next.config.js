680559ca0e41c337bec65fd16649159300f413f1
eddieoz <osoriojr@gmail.com>
2023-12-09 15:56:05 +0200

    Maintenance mode

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+module.exports = {
+    redirects() {
+        return [
+            process.env.MAINTENANCE_MODE === "1"
+                ? { source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }
+                : null,
+        ].filter(Boolean);
+    }
+};
